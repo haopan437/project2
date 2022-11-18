d5b6af808b27bf5a63e2685f25ef745a2baf552a
HAO <haopan437@gmail.com>
2022-11-18 02:57:36 -0800

    project2

--- src/App.js
+++ src/App.js
@@ -4,26 +4,15 @@ import Child from "./components/childs/child";
 import Clear from "./components/childs/clear";
 class App extends Component{
 
-    constructor(props) {
-        super(props);
-        this.state = {num:0}
-    }
 
-    changeIt(num){
-        this.state.num = num
-        this.setState({num:num})
-    }
 
 
     render(){
         return (
 
             <div className="">
-                <h1>count:{this.state.num}</h1>
-                <Child num = {this.state.num} myfun={this.changeIt.bind(this)}></Child>
-                <Child num = {this.state.num} myfun={this.changeIt.bind(this)}></Child>
-                <Clear num = {this.state.num} myfun={this.changeIt.bind(this)}></Clear>
-                <Child num = {this.state.num} myfun={this.changeIt.bind(this)}></Child>
+
+
             </div>
 
         );
